b760dd85955bbc81428168afb60912cdc0884691
vajeevan <vajeevan@gmail.com>
2015-04-10 17:49:19 +0100

    first commit

---
+++ CRM/Guestsearch/Form/Search/Guestsearch.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * A custom contact search
+ */
+class CRM_Guestsearch_Form_Search_Guestsearch extends CRM_Contact_Form_Search_Custom_Base implements CRM_Contact_Form_Search_Interface {
+
+  function __construct(&$formValues) {
+    parent::__construct($formValues);
+  }
+
+  /**
+   * Prepare a set of search fields
+   *
+   * @param CRM_Core_Form $form modifiable
+   * @return void
+   */
+  function buildForm(&$form) {
+    CRM_Utils_System::setTitle(ts('My Search Title'));
+
+
+    $form->add('text', 'name', ts('Name'), TRUE
+    );
+
+    $form->setDefaults(array(
+      'name' => '',
+    ));
+
+    /**
+     * if you are using the standard template, this array tells the template what elements
+     * are part of the search criteria
+     */
+    $form->assign('elements', array('name'));
+  }
+
+  /**
+   * Get a list of summary data points
+   *
+   * @return mixed; NULL or array with keys:
+   *  - summary: string
+   *  - total: numeric
+   */
+  function summary() {
+    return NULL;
+    // return array(
+    //   'summary' => 'This is a summary',
+    //   'total' => 50.0,
+    // );
+  }
+
+  /**
+   * Get a list of displayable columns
+   *
+   * @return array, keys are printable column headers and values are SQL column names
+   */
+  function &columns() {
+    // return by reference
+    $columns = array(
+      ts('Name') => 'sort_name',
+      ts('Phone') => 'phone',
+      ts('Email') => 'email',
+      ts('Date of visit') => CIVICRM_COLUMN,
+    );
+    return $columns;
+  }
+
+  /**
+   * Construct a full SQL query which returns one page worth of results
+   *
+   * @param int $offset
+   * @param int $rowcount
+   * @param null $sort
+   * @param bool $includeContactIDs
+   * @param bool $justIDs
+   * @return string, sql
+   */
+  function all($offset = 0, $rowcount = 0, $sort = NULL, $includeContactIDs = FALSE, $justIDs = FALSE) {
+
+    return $this->sql($this->select(), $offset, $rowcount, null, $includeContactIDs, NULL);
+  }
+
+  /**
+   * Construct a SQL SELECT clause
+   *
+   * @return string, sql fragment with SELECT arguments
+   */
+  function select() {
+    return "
+      contact_a.id                  as contact_id  ,
+      contact_a.contact_sub_type    as contact_sub_type,
+      contact_a.sort_name           as sort_name,
+      phone.phone ,
+      email.email  ,
+      date.
+    " . CIVICRM_COLUMN;
+  }
+
+  /**
+   * Construct a SQL FROM clause
+   *
+   * @return string, sql fragment with FROM and JOIN clauses
+   */
+  function from() {
+    return "
+      FROM      civicrm_contact contact_a
+      LEFT JOIN civicrm_activity_contact a ON (a.contact_id = contact_a.id)
+      LEFT JOIN civicrm_phone phone ON (phone.contact_id = contact_a.id)
+      LEFT JOIN civicrm_email email ON (email.contact_id = contact_a.id)
+      LEFT JOIN " . CIVICRM_TABLE . " date ON (date.entity_id = contact_a.id)
+    ";
+  }
+
+  /**
+   * Construct a SQL WHERE clause
+   *
+   * @param bool $includeContactIDs
+   * @return string, sql fragment with conditional expressions
+   */
+  function where($includeContactIDs = false) {
+    $params = array();
+
+    $where = "contact_a.contact_sub_type = 'Guest' AND activity_id IS NULL ORDER BY date." . CIVICRM_COLUMN . " ASC ";
+
+    return $where;
+
+  }
+
+  /**
+   * Determine the Smarty template for the search screen
+   *
+   * @return string, template path (findable through Smarty template path)
+   */
+  function templateFile() {
+    return 'CRM/Contact/Form/Search/Custom.tpl';
+  }
+
+  /**
+   * Modify the content of each row
+   *
+   * @param array $row modifiable SQL result row
+   * @return void
+   */
+
+}
---
+++ guestsearch.php
@@ -0,0 +1,111 @@
+<?php
+
+require_once 'guestsearch.civix.php';
+define('CIVICRM_TABLE','civicrm_value_date_of_first_visit_4');
+define('CIVICRM_COLUMN','date_of_first_visit_7');
+
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function guestsearch_civicrm_config(&$config) {
+  _guestsearch_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function guestsearch_civicrm_xmlMenu(&$files) {
+  _guestsearch_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function guestsearch_civicrm_install() {
+  _guestsearch_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function guestsearch_civicrm_uninstall() {
+  _guestsearch_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function guestsearch_civicrm_enable() {
+  _guestsearch_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function guestsearch_civicrm_disable() {
+  _guestsearch_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function guestsearch_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _guestsearch_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function guestsearch_civicrm_managed(&$entities) {
+  _guestsearch_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function guestsearch_civicrm_caseTypes(&$caseTypes) {
+  _guestsearch_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function guestsearch_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _guestsearch_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
